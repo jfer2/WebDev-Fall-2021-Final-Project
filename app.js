628b58d07a8340929d4d35191a715649a7911c1b
jfer2 <jfer2@pdx.edu>
2021-11-21 10:24:44 -0800

    Added early logic for 'x' to replace menu icon

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const navSlide = () => {
     const navMenu = document.querySelector('.nav-menu');
     const nav = document.querySelector('.nav-list');
+    let lines = document.querySelectorAll('.line');
 
     navMenu.addEventListener('click', () => {
+
         nav.classList.toggle('nav-active');
-        const imgElement = document.createElement('img');
-        nav.append(imgElement);
+
+        // for (line of lines) {
+        //     line.style.display = 'none';
+        // }
+
+        navMenu.classList.toggle('navMenu-active');
+
+        // for (line of lines) {
+        //     line.style.display = 'block';
+        // }
     });
 
     nav.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+        // nav.classList.toggle('nav-active');
+    });
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth >= 769) {
+            console.log('class name: ' + nav.classList);
+            if (nav.classList.contains('nav-list') && nav.classList.contains('nav-active')) {
+                nav.classList.toggle('nav-active');
+                if (navMenu.classList.contains('navMenu-active') && navMenu.classList.contains('nav-menu')) {
+                    navMenu.toggle('navMenu-active');
+                }
+            }
+        }
     });
 };
 
@@ -17,3 +39,5 @@ const navSlide = () => {
 document.addEventListener("DOMContentLoaded", () => {
     navSlide();
 });
+
+
