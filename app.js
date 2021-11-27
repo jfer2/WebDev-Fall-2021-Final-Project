8e86670a976a76f8e2f3d6cfd87691fe3c7a3980
jfer2 <jfer2@pdx.edu>
2021-11-27 09:07:08 -0800

    Fixed bug regard menu slide on device res change

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
+
 const navSlide = () => {
     const navMenu = document.querySelector('.nav-menu');
     const nav = document.querySelector('.nav-list');
     let lines = document.querySelectorAll('.line');
 
-    navMenu.addEventListener('click', () => {
 
+    navMenu.addEventListener('click', () => {
+        nav.style.display = 'flex';
         nav.classList.toggle('nav-active');
         navMenu.classList.toggle('navMenu-active');
-
     });
 
     nav.addEventListener('click', () => {
+        nav.style.display = 'flex';
         navMenu.classList.toggle('navMenu-active');
         nav.classList.toggle('nav-active');
     });
 
     window.addEventListener('resize', () => {
         if (window.innerWidth >= 769) {
+            nav.style.display = 'flex';
             if (nav.classList.contains('nav-list') && nav.classList.contains('nav-active')) {
                 nav.classList.toggle('nav-active');
             }
             if (navMenu.classList.contains('nav-menu') && navMenu.classList.contains('navMenu-active')) {
                 navMenu.classList.toggle('navMenu-active');
             }
+        } else {
+            nav.style.display = 'none';
         }
     });
 };
 
-// const adjustMainContentToWindowHeight = () => {
-//     window.addEventListener('resize', () => {
-//         if (window.innerHeight <= 768) {
-//             console.log(window.innerHeight);
-//             document.querySelector('.wrapper').height = window.innerHeight;
-//             document.querySelector('.wrapper').style.backgroundColor = 'silver';
-//         }
-//     });
-// };
-
 
 document.addEventListener("DOMContentLoaded", () => {
     navSlide();
-    adjustMainContentToWindowHeight();
 });
 
 
