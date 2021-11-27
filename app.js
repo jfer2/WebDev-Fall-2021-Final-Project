9df93f4e094dee193533bc1aad91e1df5a68e440
jfer2 <jfer2@pdx.edu>
2021-11-27 09:34:33 -0800

    Reverted back to previous app.js

--- app.js
+++ app.js
@@ -6,28 +6,23 @@ const navSlide = () => {
 
 
     navMenu.addEventListener('click', () => {
-        nav.style.display = 'flex';
         nav.classList.toggle('nav-active');
         navMenu.classList.toggle('navMenu-active');
     });
 
     nav.addEventListener('click', () => {
-        nav.style.display = 'flex';
         navMenu.classList.toggle('navMenu-active');
         nav.classList.toggle('nav-active');
     });
 
     window.addEventListener('resize', () => {
         if (window.innerWidth >= 769) {
-            nav.style.display = 'flex';
             if (nav.classList.contains('nav-list') && nav.classList.contains('nav-active')) {
                 nav.classList.toggle('nav-active');
             }
             if (navMenu.classList.contains('nav-menu') && navMenu.classList.contains('navMenu-active')) {
                 navMenu.classList.toggle('navMenu-active');
             }
-        } else {
-            nav.style.display = 'none';
         }
     });
 };
@@ -36,5 +31,3 @@ const navSlide = () => {
 document.addEventListener("DOMContentLoaded", () => {
     navSlide();
 });
-
-
